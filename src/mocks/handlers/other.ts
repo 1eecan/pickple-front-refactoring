f8713e5f0e81d6776ecbee8e4428d4c2a0542b51
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-05-10 16:56:18 +0900

    feat: positions에 대한 mock data 및 handler 추가

--- src/mocks/handlers/other.ts
+++ src/mocks/handlers/other.ts
@@ -1,1 +1,9 @@
-export const otherHandlers = [];
+import { HttpResponse, http } from 'msw';
+
+import { POSITIONS } from '@mocks/data/other';
+
+const mockGetPositions = http.get('/api/positions', () => {
+  return HttpResponse.json(POSITIONS);
+});
+
+export const otherHandlers = [mockGetPositions];
