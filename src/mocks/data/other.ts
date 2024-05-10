7c0875d6ba04a791b0491ccec250e87a870fde5c
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-05-10 16:56:18 +0900

    feat: positions에 대한 mock data 및 handler 추가

--- src/mocks/data/other.ts
+++ src/mocks/data/other.ts
@@ -1,1 +1,32 @@
-export const otherHandlers = [];
+export const POSITIONS = [
+  {
+    name: '센터',
+    acronym: 'C',
+    description: '팀의 공격과 수비에서 중추적인 역할',
+  },
+  {
+    name: '파워 포워드',
+    acronym: 'PF',
+    description: '득점, 리바운드, 수비에서의 강인한 플레이를 담당하는 역할',
+  },
+  {
+    name: '스몰 포워드',
+    acronym: 'SF',
+    description: '공격과 수비 양면에서 다재다능한 역할',
+  },
+  {
+    name: '포인트 가드',
+    acronym: 'PG',
+    description: '공격 조직과 전술적인 플레이를 주도하는 주장 역할',
+  },
+  {
+    name: '슈팅 가드',
+    acronym: 'SG',
+    description: '주로 3점슛을 통해 팀의 주요 득점 옵션을 담당하는 역할',
+  },
+  {
+    name: '포지션 없음',
+    acronym: '없음',
+    description: '포지션을 별도로 선택하지 않음',
+  },
+];
