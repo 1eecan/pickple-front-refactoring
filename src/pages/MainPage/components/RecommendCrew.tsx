d9c5ca8a720449a8fe14b6465f057a6432d9ab90
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-05-15 17:22:55 +0900

    refactor: MainPage component 매개변수 타입지정
    * CrewRanking,NearGame,RecommendCrew

--- src/pages/MainPage/components/RecommendCrew.tsx
+++ src/pages/MainPage/components/RecommendCrew.tsx
@@ -1,8 +1,13 @@
+import { NavigateFunction } from 'react-router-dom';
+
+import { EmotionJSX } from 'node_modules/@emotion/react/types/jsx-namespace';
+
 import { Button } from '@components/shared/Button';
 import { Text } from '@components/shared/Text';
 
 import { PATH_NAME } from '@constants/pathName';
 
+import { MAIN_PAGE_BUTTON_PROP_TYPE } from '../MainPage.style';
 import { MainPageSubContainer } from '../MainPage.style';
 import { MainPageNoContentItem } from './MainPageNoContentItem';
 
@@ -10,6 +15,10 @@ const RecommendCrew = ({
   filteredCrewData,
   navigate,
   MAIN_PAGE_BUTTON_PROP,
+}: {
+  filteredCrewData: EmotionJSX.Element[];
+  navigate: NavigateFunction;
+  MAIN_PAGE_BUTTON_PROP: MAIN_PAGE_BUTTON_PROP_TYPE;
 }) => {
   return (
     <MainPageSubContainer>
