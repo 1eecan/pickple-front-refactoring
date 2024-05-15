b51faed4d78a035755be2c2851c4565dca9d760f
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-05-15 17:25:21 +0900

    feat: test setup 변경
    * 테스트시에 모킹 초기화 코드 추가
    * zustand 모킹
    * window 객체 일부 함수

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,6 +1,6 @@
-// import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
 import { afterAll, afterEach, beforeAll } from 'vitest';
+import { vi } from 'vitest';
 
 import { server } from '@mocks/server';
 
@@ -8,5 +8,25 @@ beforeAll(() => server?.listen());
 afterEach(() => {
   server?.resetHandlers();
   cleanup();
+  vi.clearAllMocks();
 });
-afterAll(() => server?.close());
+afterAll(() => {
+  vi.resetAllMocks();
+  server?.close();
+});
+
+vi.mock('zustand');
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
