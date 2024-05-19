a2109c4b8affac11e5bcf8a2a74d5c3540744617
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-05-19 23:36:05 +0900

    test: mockRender 함수 작성
    * mock context를 주입하기 위한 mockRender 작성

--- src/tests/mockRender.tsx
+++ src/tests/mockRender.tsx
@@ -1,4 +1,5 @@
-import { ReactElement, ReactNode } from 'react';
+import { ReactElement, ReactNode, Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import { ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -6,24 +7,29 @@ import { render } from '@testing-library/react';
 
 import { theme } from '@styles/theme';
 
-const queryClient = new QueryClient();
+const createQueryClient = () => new QueryClient();
 
 interface MockRenderOptions {
   wrapper?: React.FC<{ children: ReactNode }>;
 }
 
-export const mockRender = (
+export const mockRender = async (
   component: ReactElement,
   options?: MockRenderOptions
 ) => {
   const { wrapper: PageProvider } = options || {};
+  const queryClient = createQueryClient();
 
   const Wrapper = ({ children }: { children: ReactNode }) => (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        {PageProvider ? <PageProvider>{children}</PageProvider> : children}
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ErrorBoundary fallback={<div>Testing went wrong.</div>}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <Suspense fallback={<div>Loading...</div>}>
+            {PageProvider ? <PageProvider>{children}</PageProvider> : children}
+          </Suspense>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 
   return render(component, { wrapper: Wrapper });
